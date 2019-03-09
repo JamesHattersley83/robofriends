4e9d69623aa1d2f83d26dcfb784ab1148722f907
jameshattersley83 <jhatts@hotmail.co.uk>
2019-03-09 22:06:36 +0000

    added state to app.js

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange }) => {
 	return (
 		<div className="ma3 pa2">
-			<input className="pa3 ba" type="search" placeholder="Search robots..." />
+			<input className="pa3 ba" 
+			type="search" 
+			placeholder="Search robots..." 
+			onChange = {searchChange}/>
 		</div>
+
 	)
 }
 
