9d52ac4aec5f0f0d60cc7b2fdc2e7cc956d72775
jameshattersley83 <jhatts@hotmail.co.uk>
2019-03-09 21:24:59 +0000

    created searchbox component

---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = () => {
+	return (
+		<div className="ma3 pa2">
+			<input className="pa3 ba" type="search" placeholder="Search robots..." />
+		</div>
+	)
+}
+
+export default SearchBox;
