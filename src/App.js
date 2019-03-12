12cfe533ce8c7604f2dba7a8875092cec92bec42
jameshattersley83 <jhatts@hotmail.co.uk>
2019-03-12 20:44:19 +0000

    added css styles

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import { robots } from './robots';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import { robots } from "./robots";
+import "./App.css";
 
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			robots: robots,
-			searchfield: ''
-		}
-	}
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ""
+    };
+  }
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
-	}
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-		return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-	})
-		return (
-			<div className="tc">
-				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<CardList robots={filteredRobots} />
-			</div>
-		);
-	}
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f1 pa3">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
