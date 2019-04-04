b736269c0aed71ef86221899d7d7727c5fdd0f10
James Hattersley <jameshattersley83@gmail.com>
2019-04-04 21:53:31 +0100

    Updates

---
+++ precache-manifest.830271818e157e0eceea8353b8922642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "22e05b86a5bc56cf75b9ab7683fa4309",
+    "url": "/robofriends/static/media/Alien Robot.22e05b86.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "31f45c1d1c8a7fbe737f",
+    "url": "/robofriends/static/js/main.31f45c1d.chunk.js"
+  },
+  {
+    "revision": "f2f233af41574b290256",
+    "url": "/robofriends/static/js/2.f2f233af.chunk.js"
+  },
+  {
+    "revision": "31f45c1d1c8a7fbe737f",
+    "url": "/robofriends/static/css/main.926bc059.chunk.css"
+  },
+  {
+    "revision": "f2f233af41574b290256",
+    "url": "/robofriends/static/css/2.affbd00a.chunk.css"
+  },
+  {
+    "revision": "1035faf8327e5a6f82df524493afad0d",
+    "url": "/robofriends/index.html"
+  }
+];
